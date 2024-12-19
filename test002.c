1c4e9945c3532d01f33aefb883ee8c355a2379d3
wangwenc <2729420403@qq.com>
2024-12-19 16:42:07 +0800

    modify 02.c and 03.c

--- test002.c
+++ test002.c
@@ -1,10 +1,16 @@
 //写一个二分法查找函数
 #include <stdio.h>
+/**
+ * @note 二分法查找
+ * @param arr 待查找数组
+ * @param len 数组长度
+ */
 int binary_search(int arr[], int len, int key)
 {
-    int left = 0;
-    int right = len - 1;
-    int mid;
+    int left = 0; //左边界
+    int right = len - 1; //右边界
+    int mid = 0; //中间位置
+
     while (left <= right)
     {
         mid = left + (right - left) / 2;
@@ -22,4 +28,27 @@ int binary_search(int arr[], int len, int key)
         }
     }
     return -1;
+}
+
+//写一个选择排序的函数
+void select_sort(int arr[], int len)
+{
+    int i, j, min, temp;
+    for (i = 0; i < len - 1; i++)
+    {
+        min = i;
+        for (j = i + 1; j < len; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        if (min != i)
+        {
+            temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
 }
--- test003.c
+++ test003.c
@@ -1,19 +1,38 @@
 //写一个冒泡排序的函数
 #include <stdio.h>
+/**
+ * @note 冒泡排序
+ * @param arr 待排序数组
+ * @param len 数组长度
+ */
 void bubble_sort(int arr[], int len)
 {
-    int i, j, temp;
+    int i, j, temp; //i,j为循环变量，temp为临时变量
     for (i = 0; i < len - 1; i++)
     {
-        for (j = 0; j < len - 1 - i; j++)
+        for (j = 10; j < len - 1 - i; j++)
         {
-            if (arr[j] > arr[j + 1])
+            if (arr[j] > arr[j + 2])
             {
                 temp = arr[j];
-                arr[j] = arr[j + 1];
+                arr[j] = arr[j + 2];
                 arr[j + 1] = temp;
             }
         }
     }
 }
 
+//写一个插入排序的函数
+void insert_sort(int arr[], int len)
+{
+    int i, j, temp;
+    for (i = 1; i < len; i++)
+    {
+        temp = arr[i];
+        for (j = i; j > 0 && arr[j - 1] > temp; j--)
+        {
+            arr[j] = arr[j - 1];
+        }
+        arr[j] = temp;
+    }
+}
